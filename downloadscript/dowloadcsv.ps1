7fae82a18ec84a0f0eae9457604176acc3001f1c
kavyasukumar <kavya.sukumar@hotmail.com>
2014-07-07 13:46:12 -0400

    adding script to download csv file

---
+++ downloadscript/dowloadcsv.ps1
@@ -0,0 +1,18 @@
+param(
+    $path ="c:\temp"
+    )
+
+$olFolderInbox = 6
+
+$objOutlook = new-object -com outlook.application; 
+$ns = $objOutlook.GetNameSpace("MAPI");
+$inbox = $ns.GetDefaultFolder($olFolderInbox)
+
+$targetfolder = $inbox.Folders | where-object { $_.name -eq "courtscsv" }
+$message = $targetfolder.Items | where-object {$_.subject -like "*CSV*" -and $_.receivedTime.dayofyear -eq (get-date).dayofyear}
+
+$message.attachments|foreach{
+	$filepath = join-path $path $_.filename
+	write-host "saving file to " $filepath
+	$_.saveasfile($filepath)
+}
