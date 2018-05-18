1298900fae2353653a7aee98be4a1358a4b4de88
unknown <mstucka@NWPBPBP0DPC2334.cmg.int>
2018-05-18 15:05:38 -0400

    Email changes

--- script/creds.py
+++ script/creds.py
@@ -1,10 +1,8 @@
- access = {
-	"emailfrom": '"Some User" <someuser@somewhere.com>',
-	"emailto": "someuser@somewhere.com,ivorytower@someplace.edu",
-	"emailserver": "smtp.somewhere.com",
-	"gmailaccount": "someuser@gmail.com",
-	"gmailpassword": "supersecret"
+    # "emailto": ["MStucka@pbpost.com", "stucka@gmail.com", "pbpostdata@gmail.com"],
+access = {
+    "emailto": ["JEngelhardt@pbpost.com", "JMusgrave@pbpost.com", "DDuret@pbpost.com", "hwinston@pbpost.com", "JWhigham@pbpost.com", "JColeman@pbpost.com", "JBisognano@pbpost.com", "CStapleton@pbpost.com", "ref@pbpost.com", "MStucka@pbpost.com", "KWebb@pbpost.com", "ohitchcock@pbpost.com"],
 	"gmailbox": "Courtssearch"	
- }
+}
  
- "pbpostdata@gmail.com","gekken500"
+	#
+ 
--- script/sendemail.py
+++ script/sendemail.py
@@ -1,55 +1,31 @@
 #!/usr/bin/python
-#import smtplib
-#from email.mime.text import MIMEText
-#from email.mime.multipart import MIMEMultipart
-import sys
 from datetime import date
-import subprocess
+# import subprocess
 import os
 import creds
 
-## Switched to using BLAT from
-## http://sourceforge.net/projects/blat/
+import emails
 
-## If shifting to Unix, try mutt.
-
-server = creds.access['emailserver']
-fromaddr = creds.access['emailfrom']
+emailuser = os.environ["GOOGLEADDRESS"]
+emailpassword = os.environ["GOOGLEPASSWORD"]
+emailname = os.environ["GOOGLENAME"]
+emailfrom = emailname + " <" + emailuser + ">"
 toaddr = creds.access['emailto']
 
-## Uncomment below for testing #########################################
-#toaddr = "mstucka@pbpost.com"
-
 msgsource = "..\datafiles\email.htm"
-msgattach = "..\datafiles\defendants_" + str(date.today().strftime("%Y-%m-%d")) + ".csv"
-toaddr = toaddr.replace(" ", "")			# Get rid of extra spaces
-subject="Courts search " + str(date.today().strftime("%Y-%m-%d"))
+with open(msgsource, "r") as f:
+    html = f.read()
 
-if not (os.path.isfile(msgsource) and os.path.isfile(msgattach)):
-	print "Some of the files are missing. Not emailing."
+msgattachpre = "../datafiles/"
+msgattach = "defendants_" + str(date.today().strftime("%Y-%m-%d")) + ".csv"
+# toaddr = toaddr.replace(" ", "")            # Get rid of extra spaces
+subject = "Courts search " + str(date.today().strftime("%Y-%m-%d"))
+
+if not (os.path.isfile(msgsource) and os.path.isfile(msgattachpre + msgattach)):
+    print "Some of the files are missing. Not emailing."
 else:
-	print "All files found. Let's try to send an email."
-	subprocess.check_call(["./bin/blat.exe", msgsource, "-attacht", '"' + msgattach + '"', "-to", toaddr, "-f", fromaddr, "-subject", subject, "-server", server ])
-
-## Python emails were kind of hinky. Switched to Blat, disabled this.
-# msg = MIMEMultipart()
-# msg['Subject'] = "Courts search " + str(date.today().strftime("%Y-%m-%d"))
-# msg['From'] = fromaddr
-# msg['To'] = toaddr
-
-
-# #Send email.htm
-# filehandler = open(msgsource, 'r')
-# contents = MIMEText(filehandler.read(), 'html')
-# filehandler.close()
-# msg.attach(contents)
-
-# #Send the defendants csv file
-# filehandler = open(msgattach, 'r')
-# contents = MIMEText(filehandler.read(), 'text')
-# filehandler.close()
-# msg.attach(contents)
-
-# s=smtplib.SMTP(server)
-# s.sendmail(msg['From'],msg['To'],msg.as_string())
-# s.quit()
+    print "All files found. Let's try to send an email."
+    # subprocess.check_call(["./bin/blat.exe", msgsource, "-attacht", '"' + msgattach + '"', "-to", toaddr, "-f", fromaddr, "-subject", subject, "-server", server ])
+    message = emails.html(html=html, subject=subject, mail_from=emailfrom)
+    message.attach(filename=msgattach, content_disposition="inline", data=open(msgattachpre + msgattach, "r"))
+    message.send(to=(toaddr), smtp={"host":"smtp.gmail.com", "port":465, "ssl":True, "user":emailuser, "password":emailpassword} )
