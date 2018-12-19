7939c10f985025b8f3950c6862c7d6114d177fac
Stucka <mstucka@cmg.com>
2018-12-19 10:05:56 -0500

    Ooops

--- script/creds.py
+++ script/creds.py
@@ -1,6 +1,6 @@
     # "emailto": ["MStucka@pbpost.com", "stucka@gmail.com", "pbpostdata@gmail.com"],
 access = {
-    "emailto": ["telia@pbpost.com", "JMusgrave@pbpost.com", "DDuret@pbpost.com", "hwinston@pbpost.com", "JWhigham@pbpost.com", "JColeman@pbpost.com", "JBisognano@pbpost.com", "CStapleton@pbpost.com", "ref@pbpost.com", "MStucka@pbpost.com", "ohitchcock@pbpost.com", "jmillian@pbpost.com"],
+    "emailto": ["telia@pbpost.com", "JMusgrave@pbpost.com", "DDuret@pbpost.com", "hwinston@pbpost.com", "JWhigham@pbpost.com", "JColeman@pbpost.com", "JBisognano@pbpost.com", "CStapleton@pbpost.com", "ref@pbpost.com", "MStucka@pbpost.com", "ohitchcock@pbpost.com", "jmilian@pbpost.com"],
 	"gmailbox": "Courtssearch"	
 }
  
